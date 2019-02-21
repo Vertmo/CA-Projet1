4212892b36bead9611788ed1e97aec752225b398
Vertmo <basile.pesin@gmail.com>
2019-02-21 17:09:47 +0100

    Basic project structure

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # CA-Projet1
 Le premier projet de CA - une mini ZAM !
+
+## Dependances:
+* OCaml (v4.06.1)
+* [dune](https://github.com/ocaml/dune) pour
---
+++ src/minizam.ml
@@ -0,0 +1,18 @@
+let usage = "usage: " ^ Sys.argv.(0) ^ " [-parse] [-eval] <input_file>"
+
+type step = Parse | Eval
+
+let step = ref Eval
+
+let speclist = [
+  ("-parse", Arg.Unit (fun () -> step := Parse), ": only parse the program");
+  ("-eval", Arg.Unit (fun () -> step := Eval), ": evaluate the program (default)")
+]
+
+let main filename step =
+  let ic = open_in filename in
+  let bc = Parser.parse ic in
+  if(step = Parse) then print_endline (Bytecode.string_of_bc bc)
+  else ignore (Eval.eval_bc bc)
+
+let _ = Arg.parse speclist (fun x -> main x !step) usage
