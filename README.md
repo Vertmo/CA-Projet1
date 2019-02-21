50951b9cdd1233ddd1311dd68b68164f849108ba
Vertmo <basile.pesin@gmail.com>
2019-02-21 19:36:25 +0100

    Parsing for unary functions done

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Le premier projet de CA - une mini ZAM !
 ## Dependances:
 * OCaml (v4.06.1)
 * [dune](https://github.com/ocaml/dune) pour
+
+## Utilisation:
+* `dune build src/minizam.exe` pour compiler
+* `./_build/default/src/minizam.exe -parse <bytecode>` pour parser un bytecode et afficher le contenu
+* `./_build/default/src/minizam.exe <bytecode>` pour executer un bytecode
--- src/bytecode.ml
+++ src/bytecode.ml
@@ -1,1 +1,49 @@
-let string_of_bc _ = ""
+type prim = Add | Sub | Mult | Div
+          | Or | And | Not
+          | Eq | Diff | Lt | Lte | Gt | Gte
+          | Print
+
+let string_of_prim = function
+  | Add -> "+" | Sub -> "-" | Mult -> "*" | Div -> "/"
+  | Or -> "or" | And -> "and" | Not -> "not"
+  | Eq -> "="  | Diff -> "<>"
+  | Lt -> "<" | Lte -> "<=" | Gt -> ">" | Gte -> ">="
+  | Print -> "print"
+
+type opcode = CONST of int
+            | PRIM of prim
+            | BRANCH of string
+            | BRANCHIFNOT of string
+            | PUSH
+            | POP
+            | ACC of int
+            | ENVACC of int
+            | CLOSURE of string * int
+            | APPLY of int
+            | RETURN of int
+            | STOP
+
+let string_of_opcode = function
+  | CONST n -> Printf.sprintf "CONST %d" n
+  | PRIM p -> Printf.sprintf "PRIM %s" (string_of_prim p)
+  | BRANCH l -> Printf.sprintf "BRANCH %s" l
+  | BRANCHIFNOT l -> Printf.sprintf "BRANCHIFNOT %s" l
+  | PUSH -> "PUSH"
+  | POP -> "POP"
+  | ACC i -> Printf.sprintf "ACC %d" i
+  | ENVACC i -> Printf.sprintf "ENVACC %d" i
+  | CLOSURE (l, n) -> Printf.sprintf "CLOSURE %s,%d" l n
+  | APPLY n -> Printf.sprintf "APPLY %d" n
+  | RETURN n -> Printf.sprintf "RETURN %d" n
+  | STOP -> "STOP"
+
+type ins =
+  | Anon of opcode
+  | Labeled of string * opcode
+
+let string_of_ins = function
+  | Anon o -> Printf.sprintf "\t%s" (string_of_opcode o)
+  | Labeled (l, o) -> Printf.sprintf "%s:\t%s" l (string_of_opcode o)
+
+let string_of_bc bc =
+  String.concat "\n" (List.map string_of_ins bc)
